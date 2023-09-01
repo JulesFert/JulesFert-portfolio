cb424a7549767f6ad19103fc0cd68cab93a85e8b
JulesFert <jfert.lyon@gmail.com>
2023-09-01 21:31:26 +0200

    update not deployed on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://JulesFert.github.io',
-  base: '/JulesFert.github.io',
-});
+export default defineConfig({});
